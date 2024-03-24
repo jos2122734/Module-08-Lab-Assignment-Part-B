cd87d288a3cb5cfc14abb277d0645c07add54fa3
Joshua Richter <jos2122734@maricopa.edu>
2024-03-24 13:04:09 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Personalized Greeting in Python
+
+A simple interaction where you ask for the user's name and greet them.
---
+++ hello.py
@@ -0,0 +1,5 @@
+# Ask for the user's name
+user_name = input("Hello! What's your name?: ")
+
+# Greet the user personally
+print(f"Hello, {user_name}!")
